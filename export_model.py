cf7f3092a7896006431ae1643b2b3b6a9f502d23
thienngoanpc <116271335+thienngoanpc@users.noreply.github.com>
2026-01-29 08:02:31 +0700

    Update export_model.py

--- export_model.py
+++ export_model.py
@@ -2,5 +2,7 @@ import torch
 from model import FetchPolicy
 
 model = FetchPolicy()
+dummy_obs = torch.randn(1, 50)
+
 torch.save(model.state_dict(), "fetch_policy.pt")
-print("Model saved")
+print("Model saved: fetch_policy.pt")
